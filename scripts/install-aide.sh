109b990281e38037f783d8522d3eff3278ea8ad9
3nnui <lex@immerda.ch>
2023-07-03 01:07:19 +0200

    refactor: format

--- scripts/install-aide.sh
+++ scripts/install-aide.sh
@@ -3,37 +3,36 @@
 # Color codes for fancy output
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-RED='\033[0;31m'     
+RED='\033[0;31m'
 NC='\033[0m' # No color
 
 # Function to print colored messages
 print_success() {
-    echo -e "${GREEN}[SUCCESS]${NC} $1"
+  echo -e "${GREEN}[SUCCESS]${NC} $1"
 }
 
 print_info() {
-    echo -e "${YELLOW}[INFO]${NC} $1"
+  echo -e "${YELLOW}[INFO]${NC} $1"
 }
 
 print_error() {
-    echo -e "${RED}[PROBLEM]${NC} $1"
+  echo -e "${RED}[PROBLEM]${NC} $1"
 }
 
 # Check if the script is being run with bash
 if [[ -n "$BASH_VERSION" ]]; then
-    print_success "This script is being run with bash."
+  print_success "This script is being run with bash."
 else
-    print_error "This script is not being run with bash."
-    exit 1
+  print_error "This script is not being run with bash."
+  exit 1
 fi
-
 
 # Check if the script is being run with bash
 if [[ -n "$BASH_VERSION" ]]; then
-    print_success "This script is being run with bash."
+  print_success "This script is being run with bash."
 else
-    print_error "This script is not being run with bash."
-    exit 1
+  print_error "This script is not being run with bash."
+  exit 1
 fi
 
 # Install AIDE package
@@ -51,10 +50,9 @@ sudo mv /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz
 # Check if AIDE configuration file permissions are secure
 CONFIG_FILE="/etc/aide.conf"
 if [[ $(stat -c %a "$CONFIG_FILE") -gt 600 ]]; then
-    print_error "The AIDE configuration file ($CONFIG_FILE) permissions are not secure. Please set the file permissions to 600 or stricter."
-    exit 1
+  print_error "The AIDE configuration file ($CONFIG_FILE) permissions are not secure. Please set the file permissions to 600 or stricter."
+  exit 1
 fi
 
 print_success "AIDE has been installed and configured successfully."
 print_info "run aide --help for usage guide"
-
