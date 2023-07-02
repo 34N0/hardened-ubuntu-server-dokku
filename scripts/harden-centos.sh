1f87590a34fc4156a2fb31d85a3b90cce3762930
3nnui <lex@immerda.ch>
2023-07-03 00:32:44 +0200

    fix: type

--- scripts/harden-centos.sh
+++ scripts/harden-centos.sh
@@ -175,7 +175,7 @@ sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/g' /etc/ssh/sshd_conf
 sed -i 's/#PermitUserEnvironment no/PermitUserEnvironment no/g' /etc/ssh/sshd_config
 sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 300/g' /etc/ssh/sshd_config
 sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 0/g' /etc/ssh/sshd_config
-print_info "Ciphers aes128-ctr,aes192-ctr,aes256-ctr" >>/etc/ssh/sshd_config
+echo "Ciphers aes128-ctr,aes192-ctr,aes256-ctr" >>/etc/ssh/sshd_config
 chown root:root /etc/ssh/sshd_config
 chmod 600 /etc/ssh/sshd_config
 
