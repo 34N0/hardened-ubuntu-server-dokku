3694331f41177e56c585aa2933c207fef02814fd
34N0 <lex@immerda.ch>
2023-10-20 02:34:03 +0200

    feat: remove hardening scripts

--- scripts/install-dokku.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "download the installation script"
-wget -NP . https://dokku.com/install/v0.32.0/bootstrap.sh
-
-echo "run the installer"
-sudo DOKKU_TAG=v0.32.0 bash bootstrap.sh
-
-echo "dokku installed. You still need to configure your server domain & add your ssh key to dokku user"
